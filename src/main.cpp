87fe095f6b2b940cb935acb2b1adc8d33e85ac96
Yosof <yosofsamra321@gmail.com>
2026-01-18 04:04:32 -0700

    add glfw, catch2, and imgui via cmake fetchcontent

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,67 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	// Initialize GLFW
+	if(!glfwInit()) return -1;
+
+	// Create a window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, World!", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Load OpenGL functions using GLAD
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set up ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+
+	// Set up backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	// Main Loop
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		// Start ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// Create UI
+		ImGui::Begin("Hello, World!");
+		ImGui::Text("This is a text :).");
+		if(ImGui::Button("Click This!")) {
+			// Button Click
+		}
+		ImGui::End();
+
+		// Rendering
+		ImGui::Render();
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	glfwTerminate();
 	return 0;
 }
